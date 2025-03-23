e94d32359ef710511f4f87b3a8db3d77301cb88d
Sarah Beirigo <sarahcbeirigo@gmail.com>
2025-03-23 18:46:43 -0300

    feat: adicionar footer

--- hotelTransilvania/hotelTransilvania/settings.py
+++ hotelTransilvania/hotelTransilvania/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
 		'django.contrib.sessions',
 		'django.contrib.messages',
 		'django.contrib.staticfiles',
-		'app',  # adicionar essa linha
+		'app',  
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'hotelTransilvania.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,7 +67,6 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'hotelTransilvania.wsgi.application'
 
 
@@ -116,7 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT =  BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
