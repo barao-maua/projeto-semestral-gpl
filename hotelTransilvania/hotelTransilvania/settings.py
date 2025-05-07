8bba765eb18c4de7cf4594b36ab3abe7593919c9
Sara M Santos <saramiranda201814@gmail.com>
2025-05-07 15:49:41 -0300

    conexao com o banco de dados

--- hotelTransilvania/hotelTransilvania/settings.py
+++ hotelTransilvania/hotelTransilvania/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'hotelTransilvania.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hoteltransilvania',
+        'USER': 'root',
+        'PASSWORD': 'nrcpc608',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
